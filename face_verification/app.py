aa796f901fe0c97f61a81e8c8858b50db4b2dbaa
okoliechykwuka <forbeseuroupe@gmail.com>
2022-12-20 09:11:15 +0100

    modified

--- face_verification/app.py
+++ face_verification/app.py
@@ -1,18 +1,12 @@
 #python3 -m flask run
-from flask import Flask, request, redirect, jsonify
+from flask import Flask, request, jsonify
 import os
+import binascii
 import markdown
 from flask_cors import CORS
 import logging
-import numpy as np
-import cv2
-import imutils
-import urllib
-from skimage import io 
-import re
-import urllib
 from authentication.auth import check_auth_header
-from face_model import compare_face, get_selfie_encoding, get_bvn_encoding
+from face_model import compare_face
 
 LOG_FILENAME = "face-recognition.log"
 logging.basicConfig(
@@ -23,25 +17,10 @@ app = Flask(__name__)
 CORS(app)
 app.debug = True
 
-ALLOWED_EXTENSIONS = ['png', 'jpg', 'jpeg']
-
-def read_selfie_url(url):
-    # pattern = pattern = re.compile(r'(?<=_)[a-z_]+(?=_)', re.IGNORECASE)
-    # sequence = url
-    # name = pattern.search(sequence).group()
-    img = io.imread(url)
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-    bgr_image = imutils.resize(img, width=500)
-    return bgr_image #name
-
-def read_bvn_url(url):
-    img = io.imread(url)
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-    bgr_image = imutils.resize(img, width=500)
-    return bgr_image
+ALLOWED_EXTENSIONS = ['data']
 
 def allowed_file(url):
-    return '.' in url and url.rsplit('.', 1)[-1].lower() in ALLOWED_EXTENSIONS
+    return ';' in url and url.rsplit(':', 1)[0].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def index():
@@ -63,38 +42,28 @@ def face_verify():
     if not validate_auth_header[0]:
        return validate_auth_header[1]
 
-    # verify = None
-    # try:
-    if request.method == 'POST':
-        if request.is_json:
-            # print('Request is a JSON format.')
-            json_data = request.get_json(cache=False)
-            # get('url1', None)
-            url1 = json_data['bvnImageUrl']
-            #get('url2', None)
-            url2 = json_data['selfieUrl']
-            
-            if not (allowed_file(url1)) or not (allowed_file(url2)):
-                return { 'status' : None,
-                        'distance': None,
-                        'message':  "Invalid JSON File"
-              
-                      }, 400
+    try:
+        if request.method == 'POST':
+            if request.is_json:
+                # print('Request is a JSON format.')
+                json_data = request.get_json(cache=False)
+                # get('url1', None)
+                url1 = json_data['NinImageUrl']
+                #get('url2', None)
+                url2 = json_data['selfieUrl']
+                if not (allowed_file(url1)) or not (allowed_file(url2)):
+                    return { 'status' : None,
+                            'distance': None,
+                            'message':  "Invalid JSON File"
+                
+                        }, 400
 
-            if allowed_file(url1) and allowed_file(url2):
-                bvnImageUrl = read_bvn_url(url1)
-                selfieUrl = read_selfie_url(url2)
-                selfie_image = get_selfie_encoding(selfieUrl)
-                bvn_image =  get_bvn_encoding(bvnImageUrl)
-
-                verify = compare_face(selfie_image, bvn_image)
-# except urllib.error.HTTPError as  error:
-    #     print(error)
-
-            return jsonify(verify)
+                if allowed_file(url1) and allowed_file(url2):
+                    verify = compare_face(url1, url2)
+                
+                return jsonify(verify)
+    except (binascii.Error,TypeError) as e:
+        print('Invalid base64-encoded string', e)
     
-
-
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5002, debug=True)
--- face_verification/face_model.py
+++ face_verification/face_model.py
@@ -1,70 +1,110 @@
-import face_recognition as fr
-import os
-import cv2
-import numpy as np
-import imutils
-import re
-from skimage import io
+# import face_recognition as fr
+# import os
+# import cv2
+# import numpy as np
+# import imutils
+# import re
+# from skimage import io
 
 
-TOLERANCE = 0.49
-MODEL = 'cnn'  # default: 'hog', other one can be 'cnn' - CUDA accelerated (if available) deep-learning pretrained model
+# TOLERANCE = 0.49
+# MODEL = 'cnn'  # default: 'hog', other one can be 'cnn' - CUDA accelerated (if available) deep-learning pretrained model
 
-# get captured face encodings and names
-def get_selfie_encoding(image):
+# # get captured face encodings and names
+# def get_selfie_encoding(image):
 
-    locations = fr.face_locations(image, model=MODEL)
-    if len(locations) == 0:
-        return []
-    try:
-        camera_encoding = fr.face_encodings(image, locations)[0]
-    except IndexError:
-        print('The face was not captured rightly, retake the image') 
+#     locations = fr.face_locations(image, model=MODEL)
+#     if len(locations) == 0:
+#         return []
+#     try:
+#         camera_encoding = fr.face_encodings(image, locations)[0]
+#     except IndexError:
+#         print('The face was not captured rightly, retake the image') 
 
-    return camera_encoding
-# known_names, selfie_face_encoding = get_selfie_encoding('test_images/simon.jpg')
-# print(camera_face_encoding)
+#     return camera_encoding
+# # known_names, selfie_face_encoding = get_selfie_encoding('test_images/simon.jpg')
+# # print(camera_face_encoding)
 
-def get_bvn_encoding(image):
-    # Load image
-    # print(f'Filename {filename}', end='')
+# def get_bvn_encoding(image):
+#     # Load image
+#     # print(f'Filename {filename}', end='')
 
-    # This time we first grab face locations - we'll need them to draw boxes
-    locations = fr.face_locations(image, model=MODEL)
+#     # This time we first grab face locations - we'll need them to draw boxes
+#     locations = fr.face_locations(image, model=MODEL)
 
-    # # Now since we know loctions, we can pass them to face_encodings as second argument
-    # # Without that it will search for faces once again slowing down whole process
-    encodings = fr.face_encodings(image, locations)[0]
-    # # We passed our image through face_locations and face_encodings, so we can modify it
-    # # First we need to convert it from RGB to BGR as we are going to work with cv2
-    return encodings
-# bvn_encoding = get_bvn_encoding('test_images/akon_15.jpeg')
+#     # # Now since we know loctions, we can pass them to face_encodings as second argument
+#     # # Without that it will search for faces once again slowing down whole process
+#     encodings = fr.face_encodings(image, locations)[0]
+#     # # We passed our image through face_locations and face_encodings, so we can modify it
+#     # # First we need to convert it from RGB to BGR as we are going to work with cv2
+#     return encodings
+# # bvn_encoding = get_bvn_encoding('test_images/akon_15.jpeg')
 
-def compare_face(camera_encoding, bvn_encoding):
+# def compare_face(camera_encoding, bvn_encoding):
 
-    if len(camera_encoding) == 0 or len(bvn_encoding) == 0:
-        return { 'status' : None,
-                        'distance': None,
-                        'message':  'Retake selfie image'
+#     if len(camera_encoding) == 0 or len(bvn_encoding) == 0:
+#         return { 'status' : None,
+#                         'distance': None,
+#                         'message':  'Retake selfie image'
               
-                      }
+#                       }
         
        
    
     
-    results = fr.compare_faces([camera_encoding], bvn_encoding, TOLERANCE)
-    distance = fr.api.face_distance([camera_encoding], bvn_encoding).tolist() 
-    image_distance = round(distance[0],3)
-    verification = { 'status' : bool(results[0]),
-                        'distance': image_distance,
-                        'message': 'verified successfully'
-                      }
+#     results = fr.compare_faces([camera_encoding], bvn_encoding, TOLERANCE)
+#     distance = fr.api.face_distance([camera_encoding], bvn_encoding).tolist() 
+#     image_distance = round(distance[0],3)
+#     verification = { 'status' : bool(results[0]),
+#                         'distance': image_distance,
+#                         'message': 'verified successfully'
+#                       }
 
-    return verification
+#     return verification
 
 
 # verification = compare_face(selfie_face_encoding, bvn_encoding)
 # print(verification)
+
+from PIL import Image
+import base64
+import numpy
+from io import BytesIO
+import cv2
+import face_recognition
+import time
+TOLERANCE = 0.5
+
+def decode_image(base64_image):
+    encoded_image = base64_image.split(",")[1]
+    decoded_image = base64.b64decode(encoded_image)
+    img = Image.open(BytesIO(decoded_image))#.convert('LA')
+    pixels = numpy.asarray(img, dtype='uint8')
+    resized_image = cv2.resize(pixels, (0, 0), fx=0.25, fy=0.25)
+    encodings = face_recognition.face_encodings(resized_image)[0]
+
+    return encodings
+
+def compare_face(camera_byte, nin_byte):
+    start_time = time.time()
+    camera_image = decode_image(camera_byte)
+    nin_image = decode_image(nin_byte)
+    print(nin_image.shape)
+    results = face_recognition.compare_faces([camera_image], nin_image,TOLERANCE)
+    if len(camera_image) == 0 or len(nin_image) == 0:
+        print("--- %s seconds ---" % (time.time() - start_time))
+        return { 'status' : None,
+                        'distance': None,
+                        'message':  'Retake selfie image'
+                      }
+    else:
+        distance = face_recognition.api.face_distance([camera_image], nin_image).tolist() 
+        image_distance = round(distance[0],3)
+        print("--- %s seconds ---" % (time.time() - start_time))
+        return { 'status' : bool(results[0]),
+                            'distance': image_distance,
+                            'message': 'verified successfully'
+                        }
 
 
 
